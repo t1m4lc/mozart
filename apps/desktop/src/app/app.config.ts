af8d1740d847351efe425c88d854bdddfa8affdb
Timothy Alcaide <timothy.alcaide@gmail.com>
2026-05-10 02:28:24 +0200

    chore: add tauri rust app

--- apps/desktop/src/app/app.config.ts
+++ apps/desktop/src/app/app.config.ts
@@ -2,14 +2,14 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideTheme } from '@mozart/shared-util-theme';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
-    provideTheme({}),
+    provideRouter(appRoutes, withHashLocation()),
+    provideTheme(),
   ],
 };
